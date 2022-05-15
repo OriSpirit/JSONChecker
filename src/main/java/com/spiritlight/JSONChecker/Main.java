095133def941b0ef5ae33ce5d2ff4637bd279296
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-05-15 13:35:53 +0800

    Changed JSONChecker: Execute with -b to output scanned files

--- src/main/java/com/spiritlight/JSONChecker/Main.java
+++ src/main/java/com/spiritlight/JSONChecker/Main.java
@@ -1,14 +1,17 @@
 package com.spiritlight.JSONChecker;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.Objects;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.FilenameUtils;
 
 public class Main {
+    static boolean b;
     public static void main(String[] args) {
+        if(args.length != 0)
+            if(Objects.equals(args[0], "-s"))
+                b = true;
         String path = System.getProperty("user.dir");
         System.out.println("Working in directory " + path);
         File d = new File(path);
@@ -21,14 +24,17 @@ public class Main {
             String extension = FilenameUtils.getExtension(it.getName());
             if (!it.isDirectory() && extension.equals("json")) {
                 try {
+                    if(b)
                     System.out.println("Attempting to parse file " + it.getName());
                     JsonParser.parseString(TextFileReader.readTextFile(it));
+                    if(b)
                     System.out.println("Parse successful.");
                 } catch (Exception ignored) {
                     System.out.println("Failed to parse " + it.getName());
                     System.out.println(ignored.getMessage());
                 }
             } else {
+                if (b)
                 System.out.println("Ignoring " + (extension.equals("") ? "directory" : "file") + " " + it.getName());
             }
         }
